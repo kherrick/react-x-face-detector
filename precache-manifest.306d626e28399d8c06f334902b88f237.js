7c272f948c3425b2d5566197cabf8ad7a50e1967
Karl Herrick <me@karlherrick.com>
2020-02-29 19:12:44 -0500

    Updates

---
+++ precache-manifest.306d626e28399d8c06f334902b88f237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd4660d48347804163210a6232efdf8",
+    "url": "/react-x-face-detector/index.html"
+  },
+  {
+    "revision": "fcdb26021c0b8e7e9318",
+    "url": "/react-x-face-detector/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "985e7bedf17aeb417adf",
+    "url": "/react-x-face-detector/static/js/2.50543ae6.chunk.js"
+  },
+  {
+    "revision": "a8cd82d0baac19e760c14f8a19ba832b",
+    "url": "/react-x-face-detector/static/js/2.50543ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcdb26021c0b8e7e9318",
+    "url": "/react-x-face-detector/static/js/main.40aea1d4.chunk.js"
+  },
+  {
+    "revision": "a965478ea80753419d47",
+    "url": "/react-x-face-detector/static/js/runtime-main.255526f3.js"
+  }
+]);
